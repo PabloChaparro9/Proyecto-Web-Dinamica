a647882d6f1f1f84e4df5b194f0ee0d9370dc576
Pablo Chaparro <pablo.47.114@gmail.com>
2024-12-30 10:13:51 -0300

    add movement functions

--- sketch.js
+++ sketch.js
@@ -1,3 +1,25 @@
+function goUp(){
+  Flower.velX = 0;
+  Flower.velY = -5;
+}
+function goDown(){
+  Flower.velX = 0;
+  Flower.velY = 5;
+}
+function goLeft(){
+  Flower.velX = -5;
+  Flower.velY = 0;
+  Flower.FlowerImg = SlimeLeft;
+}
+function goRight(){
+  Flower.velX = 5;
+  Flower.velY = 0;
+  Flower.FlowerImg = SlimeRight;
+}
+function resetVelocidad(){
+  Flower.velX = 0;
+  Flower.velY = 0;
+}
 function setup() {
   createCanvas(400, 400);
 }
